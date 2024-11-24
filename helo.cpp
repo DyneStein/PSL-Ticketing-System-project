ecdd176704f004f00813f0e3ad931c2b6667d4a2
DyneStein <dyssart@gmail.com>
2024-11-24 17:57:40 +0500

    Update helo.cpp
    line 6

--- helo.cpp
+++ helo.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     cout<<"Hellow I want to make this work, I want to make this shit work";
+    cout<<"This is the second change after pushing code.";
 
     return 0;
 }
